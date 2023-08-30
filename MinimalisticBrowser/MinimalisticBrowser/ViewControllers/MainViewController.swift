22cbcfc5ce7de0f67ff2800fbc9c65a5669dec09
Anst <135651132+AnstShtrm@users.noreply.github.com>
2023-08-30 16:45:19 +0200

    add app

---
+++ MinimalisticBrowser/MinimalisticBrowser/ViewControllers/MainViewController.swift
@@ -0,0 +1,14 @@
+
+import UIKit
+import Lottie
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var lottieAnimation: LottieAnimationView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+    }
+
+}
+
